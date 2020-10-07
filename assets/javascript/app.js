94cae2142b755fad026dad74616e388d8ee32f4e
Sandra <sandbar44x@gmail.com>
2020-10-07 00:31:50 -0400

    HTML and Psuedo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+// Psuedo Code
+// 
+// Start page
+// [] Start button
+// 
+// Question page
+// [] Restart timer - countdown from 30
+// [] Question
+// [] Choices
+// 
+// Results page
+// [] Pause timer
+// [] Result text
+//      [] If correct: "Correct!"
+//      [] If wrong: "Nope!"
+//      [] If timeout: "Time's up!"
+//      [] If NOT correct, also show: "The correct answer was: [answer]"
+// [] Related image/gif
+// [] Timeout to move to next question - 3 seconds?
+// 
+// Summary page
+// [] Pause timer
+// [] Result text: "All done! Here's how you did!"
+// [] Summary results
+// [] Start over button
+// 
+// Functions
+// [] How to know which question to show?
+// [] How to know which answer is wrong/correct?
+// [] How to track results (correct, incorrect, unanswered)
+// 
+// To Do
+// [] Write questions
+
+
